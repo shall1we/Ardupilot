43e20602a28e805d439f4751a8d4199aef3829d9
Andrew Tridgell <andrew@tridgell.net>
2024-06-28 10:34:46 +1000

    AP_Common: reverted QURT specific new overrides

--- libraries/AP_Common/c++.cpp
+++ libraries/AP_Common/c++.cpp
@@ -7,86 +7,6 @@
 #include <new>
 #include <AP_InternalError/AP_InternalError.h>
 
-
-#if CONFIG_HAL_BOARD == HAL_BOARD_QURT
-
-#include <new>
-
-/*
-  These are the heap access function exported by the SLPI DSP image for Qurt OS.
-*/
-extern "C" {
-    void *fc_heap_alloc(size_t size);
-    void fc_heap_free(void* ptr);
-    size_t fc_heap_size(void);
-    size_t fc_heap_usage(void);
-}
-
-void *operator new (size_t size, std::nothrow_t const &nothrow) noexcept
-{
-    if (size < 1) {
-        size = 1;
-    }
-
-    return (fc_heap_alloc(size));
-}
-
-void *operator new[](size_t size, std::nothrow_t const &nothrow) noexcept
-{
-    if (size < 1) {
-        size = 1;
-    }
-
-    return (fc_heap_alloc(size));
-}
-
-/*
-  These variants are for new without std::nothrow. We don't want to ever
-  use these from ardupilot code
- */
-void *operator new (size_t size)
-{
-    if (size < 1) {
-        size = 1;
-    }
-
-    return (fc_heap_alloc(size));
-}
-
-
-void *operator new[](size_t size)
-{
-    if (size < 1) {
-        size = 1;
-    }
-
-    return (fc_heap_alloc(size));
-}
-
-/*
-	Override delete to free up memory to correct heap
-*/
-
-// extern const AP_HAL::HAL& hal;
-
-void operator delete (void *p) noexcept
-{
-    if (p) {
-        fc_heap_free(p);
-    }
-
-    // DEV_PRINTF("Heap size: %u, heap usage: %u", fc_heap_size(), fc_heap_usage());
-}
-
-void operator delete[](void *ptr) noexcept
-{
-    if (ptr) {
-        fc_heap_free(ptr);
-    }
-}
-
-#else
-
 /*
   globally override new and delete to ensure that we always start with
   zero memory. This ensures consistent behaviour.
@@ -137,13 +57,14 @@ void * operator new[](size_t size, std::nothrow_t const &nothrow)
   These variants are for new without std::nothrow. We don't want to ever
   use this from ArduPilot code
  */
-void * operator new (size_t size)
+void * operator new(size_t size)
 {
     if (size < 1) {
         size = 1;
     }
-    return (calloc(size, 1));
+    return(calloc(size, 1));
 }
+
 
 void * operator new[](size_t size)
 {
@@ -155,30 +76,19 @@ void * operator new[](size_t size)
 
 void operator delete(void *p)
 {
-    if (p) {
-        free(p);
-    }
-}
-
-void * operator new[](size_t size)
-{
-    if (size < 1) {
-        size = 1;
-    }
-    return (calloc(size, 1));
+    if (p) free(p);
 }
 
 void operator delete[](void * ptr)
 {
-    if (ptr) {
-        free(ptr);
-    }
+    if (ptr) free(ptr);
 }
 
-#if CONFIG_HAL_BOARD != HAL_BOARD_CHIBIOS
+
+#if CONFIG_HAL_BOARD != HAL_BOARD_CHIBIOS && CONFIG_HAL_BOARD != HAL_BOARD_QURT
 /*
   wrapper around malloc to ensure all memory is initialised as zero
-  ChibiOS has its own wrapper
+  ChibiOS and QURT have their own wrappers
  */
 extern "C" {
     void *__wrap_malloc(size_t size);
